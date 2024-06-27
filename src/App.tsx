853b8dce6d1acb32e53ad6b33dc196246f226433
FBI1337 <84200204+FBI1337@users.noreply.github.com>
2024-06-27 15:58:39 +0300

    create Select

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,21 @@ import { FOOTER_NAME, HEADER_NAME, CHOOSE } from './constants';
 
 const App: React.FC = () => {
     const [isActive, setIsactive] = useState(false);
+    const [clickAtive, setActive] = useState(false);
   
     const onToggleActive = () => setIsactive(prev => !prev);
+    const onSelectActive = () => setActive(click => !click);
     
     return (
       <>
         <Header name={HEADER_NAME}/>
         <div className={styles.selectWrapper}>
           <div className={styles.diveWrapper}>{CHOOSE}</div>
-          <Select />
+          <Select
+            isActive={isActive}
+            clickAtive={clickAtive}
+            onSelectActive={onSelectActive}
+          />
         </div>
         <div className={styles.toggleWrapper}>
            <Toggle 
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import cn from 'classnames';
 import styles from './styles.module.css';
 import russia from '../Countres/russia.png';
 import usa from '../Countres/usa.png';
+import germany from '../Countres/germany.png';
+import brazil from '../Countres/brazil.png';    
+import italy from '../Countres/italy.png';
+import canada from '../Countres/canada.png';
+import argentina from '../Countres/argentina.png';
+import france from '../Countres/france.png';
+import en from '../Countres/en.png';
 
 
 
@@ -16,14 +24,67 @@ const countersList = [
         img: usa,
         countre: 'USA',
         id: 1
+    },
+
+    {
+        img: germany,
+        countre: 'Germany',
+        id: 2
+    },
+
+    {
+        img: brazil,
+        countre: 'Brazil',
+        id: 3
+    },
+
+    {
+        img: italy,
+        countre: 'Italy',
+        id: 4
+    },
+
+    {
+        img: canada,
+        countre: 'Canada',
+        id: 5
+    },
+
+    {
+        img: argentina,
+        countre: 'Argentina',
+        id: 6
+    },
+
+    {
+        img: france,
+        countre: 'France',
+        id: 7
+    },
+
+    {
+        img: en,
+        countre: 'United Kingdom',
+        id: 8
     }
 ]
 
+type IProps = {
+    clickAtive: boolean
+    isActive: boolean
+    onSelectActive: () => void
+}
 
-
-const Select:React.FC = () => {
+const Select:React.FC<IProps> =({isActive, clickAtive, onSelectActive}) => {
   return (
-    <div className={styles.selectWrapper}>
+    <div
+        className={cn(styles.selectWrapper, {
+            [styles.on]: isActive,
+            [styles.off]: !isActive,
+            [styles.click]: clickAtive,
+        })}
+        onClick={onSelectActive}
+    >
         {countersList.map(item => (
             <div className={styles.optionWrapper}>
                 <div className={styles.iconWapper}>
