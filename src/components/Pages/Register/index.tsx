9056111d4839414a92f4de71cb336841c63d83b0
FBI1337 <FBI13377@yandex.ru>
2024-09-05 00:37:49 +0300

    clonne

--- src/components/Pages/Register/index.tsx
+++ src/components/Pages/Register/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import styles from './style.module.css'
-
+// import styles from './style.module.css'
 
 
 
 const Register: React.FC = () => {
 
-
-
-
   return (
     <div>
-      <div className={styles.wrapper}>
-        Хуй
-      </div>
+
     </div>
   )
 }
