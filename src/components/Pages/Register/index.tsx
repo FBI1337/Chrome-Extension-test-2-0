9a09cb82725180de8ede964bc365f2a17b6d9b40
FBI1337 <84200204+FBI1337@users.noreply.github.com>
2024-09-05 01:48:39 +0300

    clonne

--- src/components/Pages/Register/index.tsx
+++ src/components/Pages/Register/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-// import styles from './style.module.css'
+import React, {useState} from'react'
+import styles from './style.module.css'
 
 
 
 const Register: React.FC = () => {
 
-  return (
-    <div>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  return (
+    <div className={styles.wrapper}> 
+      <header className={styles.header}>VPN</header>
+      <form>
+        <label>
+          <br className={styles.text}>
+          Username:
+          </br>
+          <input className={styles.username} value={username} onChange={e => setUsername(e.target.value)}/>
+        </label>
+        <label> 
+          <br className={styles.text}>
+          Password:
+          </br>
+          <input className={styles.password} value={password} onChange={e => setPassword(e.target.value)}/>
+        </label>
+      </form>
     </div>
+
+    // <div className={styles.wrapper}>Хуй</div>
   )
 }
 
