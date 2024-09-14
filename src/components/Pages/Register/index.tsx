178555f2d224d959a6c5c3d82e7264fbe461972b
FBI1337 <FBI13377@yandex.ru>
2024-09-14 16:26:36 +0300

    create form register

--- src/components/Pages/Register/index.tsx
+++ src/components/Pages/Register/index.tsx
@@ -1,34 +1,125 @@
-import React, {useState} from'react'
+import React, { useState } from'react'
 import styles from './style.module.css'
 
 
 
+interface RegistrFromData { 
+  username: string;
+  email: string;
+  password: string; 
+}
+
 const Register: React.FC = () => {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState<RegistrFromData>({
+    username: '',
+    email: '',
+    password: '',
+  });
 
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+
+  const handleSubmit = () => {
+    const {username, email, password} = formData;
+
+    if (!username || !email || !password) {
+      setErrorMessage('Все поля должны быть заполнены');
+      setSuccessMessage(null);
+      return;
+    }
+
+    setErrorMessage(null);
+    console.log('Отправка даных:', formData);
+
+    setSuccessMessage('Регистрация прошла успешно!');
+  };
+
+  
   return (
-    <div className={styles.wrapper}> 
-      <header className={styles.header}>VPN</header>
-      <form>
-        <label>
-          <br className={styles.text}>
-          Username:
-          </br>
-          <input className={styles.username} value={username} onChange={e => setUsername(e.target.value)}/>
-        </label>
-        <label> 
-          <br className={styles.text}>
-          Password:
-          </br>
-          <input className={styles.password} value={password} onChange={e => setPassword(e.target.value)}/>
-        </label>
-      </form>
+    <div className={styles.wrapper}>
+      Регистрация
+
+      <div className={styles.forma}>
+
+
+        <div className={styles.username}>
+            <div className={styles.laber}>
+              Имя пользователя:
+            </div>
+            <div className={styles.inputer}>
+              <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              required
+              />
+            </div>
+        </div>
+
+          <div className={styles.email}>
+
+            <div className={styles.laber}>
+              Эл.почта:
+            </div>
+
+            <div className={styles.inputer}>
+              <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+              />
+            </div>
+          </div>
+
+          <div className={styles.password}>
+
+            <div className={styles.laber}>
+              Пароль:
+            </div>
+
+            <div className={styles.inputer}>
+              <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+              />
+            </div>
+          </div>
+        
+
+        {errorMessage && <p className={styles.error}>{errorMessage}</p>}
+        {successMessage && <p className={styles.message}>{successMessage}</p>}
+      </div>
+
+      <div className={styles.buttoner}>
+        <button type="button" onClick={handleSubmit}>
+          Зарегистрироваться
+        </button>
+      </div>
+
     </div>
 
-    // <div className={styles.wrapper}>Хуй</div>
-  )
-}
+  );
+
+
+};
 
 export default Register
