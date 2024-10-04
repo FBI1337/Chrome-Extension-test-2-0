3190ea930e4f699a462999724d9c5bb622625983
FBI1337 <FBI13377@yandex.ru>
2024-10-04 17:12:45 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import App from './App.tsx'
 import './index.css'
 import store from './redux/store.ts'
 import { Provider } from 'react-redux'
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>    
     <App />
-    <SpeedInsights />
   </Provider>,
 )
